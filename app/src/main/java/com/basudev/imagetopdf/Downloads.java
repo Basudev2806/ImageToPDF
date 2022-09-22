afb7dc62256c345eef3fe33edb1bbdbeafb18688
Basudev Kar <basudev.kar.kolkata@gmail.com>
2022-09-22 21:01:10 +0530

    ImageToPDF

---
+++ app/src/main/java/com/basudev/imagetopdf/Downloads.java
@@ -0,0 +1,62 @@
+package com.basudev.imagetopdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Downloads extends AppCompatActivity implements CustomAdapter.clicked {
+
+    private ArrayList<File> arrayList = new ArrayList<>();
+    private File[] files;
+    private ListView listView;
+    private CustomAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downloads);
+        Activity activity = this;
+        listView = findViewById(R.id.list);
+        File file = activity.getExternalFilesDir("mypdfs");
+        files = file.listFiles();
+
+        arrayAdapter = new CustomAdapter(this,arrayList,this);
+        listView.setAdapter(arrayAdapter);
+
+        for (int i=0;i<files.length;i++){
+
+            arrayList.add(files[i]);
+            arrayAdapter.notifyDataSetChanged();
+
+        }
+
+    }
+
+    @Override
+    public void onclick(File file) {
+
+
+        try {
+            Uri uri = FileProvider.getUriForFile(Downloads.this, Downloads.this.getApplicationContext().getPackageName() + ".provider", file);
+            Intent intent1 = new Intent(Intent.ACTION_SEND);
+            intent1.setType("application/pdf");
+            intent1.putExtra(Intent.EXTRA_SUBJECT, "Shared");
+            intent1.putExtra(Intent.EXTRA_STREAM, uri);
+            startActivity(Intent.createChooser(intent1, "Shared doc"));
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/basudev/imagetopdf/MainActivity.java
@@ -0,0 +1,36 @@
+package com.basudev.imagetopdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(){
+
+            public void run() {
+
+                try {
+                    Thread.sleep(1200);
+
+                    Intent intent = new Intent(MainActivity.this, Second.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }.start();
+
+
+
+    }
+}
